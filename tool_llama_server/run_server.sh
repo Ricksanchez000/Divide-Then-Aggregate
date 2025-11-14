cf917e58ce48407cb1d55bdbbc030260c72c6a10
Ricksanchez000 <56523989+Ricksanchez000@users.noreply.github.com>
2025-11-13 20:21:35 -0600

    a

--- tool_llama_server/run_server.sh
+++ tool_llama_server/run_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export CUDA_VISIBLE_DEVICES=$1
-python llama_server.py --llama_model_path=$2 \
+python llama2_server.py --llama_model_path=$2 \
                           --llama_template=tool-llama-parallel \
                           --llama_device=cuda:0 \
                           --port=8899
