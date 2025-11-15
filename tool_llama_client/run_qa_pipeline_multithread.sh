31fb7b8790ec0e9166455c10094da7d43a98ead3
Ricksanchez000 <56523989+Ricksanchez000@users.noreply.github.com>
2025-11-14 22:32:37 -0600

    a

--- tool_llama_client/run_qa_pipeline_multithread.sh
+++ tool_llama_client/run_qa_pipeline_multithread.sh
@@ -3,11 +3,13 @@
 export OPENAI_KEY="EMPTY"
 export OPENAI_API_BASE=""
 
+
 # The tool directory downloaded from ToolBench
 #export TOOL_DIR=""
 export TOOL_DIR="/home/t653z659/StableToolBench/toolenv/toolenv2404_filtered/tools" #更新成了我的工具地址
 # The toolbench key we applied by filling the form (https://forms.gle/oCHHc8DQzhGfiT9r6)
-export TOOLBENCH_KEY=""
+#export TOOLBENCH_KEY=""
+export TOOLBENCH_KEY="EMPTY"
 
 export GPT_MODEL="gpt-3.5-turbo-16k"
 export SERVICE_URL="http://localhost:8080/virtual"
